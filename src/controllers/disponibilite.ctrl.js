17bc55fc256e094662046c05dfec603d9392a360
hakemi <hakemiayoub612@gmail.com>
2023-03-28 08:48:20 +0200

    all disponibilities

--- src/controllers/disponibilite.ctrl.js
+++ src/controllers/disponibilite.ctrl.js
@@ -4,6 +4,26 @@ const Creneau = db.creneau
 const Jour = db.jour
 
 
+// Get all disponibilites of a user en fonction de son id et de l'id du festival
+/*
+exports.getAllDisponibiliteOfUser = (req, res) => {
+    Disponibilite.findAll({
+        where: {
+            user_dispo: req.params.idUser
+        },
+        include: ['Creneau']
+    })
+    .then(disponibilites => {
+        res.status(200).send(disponibilites);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving disponibilites."
+        });
+    });
+}
+*/
+
 exports.getAllDisponibiliteOfUser = (req, res) => {
     Disponibilite.findAll({
         where: {
@@ -13,12 +33,24 @@ exports.getAllDisponibiliteOfUser = (req, res) => {
             model: Creneau,
             include: [{
                 model: Jour,
-                where: {festival_jour: req.params.idFestival}
+                where: {
+                    festival_jour : req.params.idFestival
+                },
             }]
+            
         }]
     })
     .then(disponibilites => {
-        res.status(200).send(disponibilites);
+        // Je souhaite renvoyer que crenau_dispo et heure_debut et heure_fin
+        const result = disponibilites.map(dispo => {
+            return {
+                id: dispo.id,
+                creneau_dispo: dispo.creneau_dispo,
+                heure_debut: dispo.Creneau.heureDebut,
+                heure_fin: dispo.Creneau.heureFin
+            }
+        })
+        res.status(200).send(result);
     })
     .catch(err => {
         res.status(500).send({
